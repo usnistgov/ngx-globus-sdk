00795dad11adc2e68b8b0733a2b04e6bbb804dac
elmiomar <omarilias.elmimouni@nist.gov>
2022-06-02 08:39:44 -0400

    move constants in a separate file

---
+++ projects/ngx-globus-sdk/src/lib/settings.ts
@@ -0,0 +1,8 @@
+export class Settings {
+
+    public static GLOBUS_TRANSFER_BASE_URL = "https://transfer.api.globus.org/v0.10";
+    public static DEFAULT_SOURCE_ENDPOINT = ""; // set this
+    public static DEFAULT_SOURCE_PATH_BASE = ""; // set this
+    public static DEFAULT_DESTINATION_PATH_BASE = ""; // set this
+
+}
